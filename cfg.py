01661bf98d01bd88fe4bbce1313ff8be0f7c89da
nmanclank <7421094+nmanclank@users.noreply.github.com>
2025-01-14 10:55:37 -0700

    Removed option to set cooldown time in favor of calculating it per cooldown

--- cfg.py
+++ cfg.py
@@ -6,9 +6,10 @@ load_dotenv()
 # Main Config file for DataHog
 AutoDelete_JSON='True' # If set to false, json files will not be deleted as they're added to db. Only applies to those using DataBase.
 USE_DATABASE='True' # If set to false, data will only be stored in json_data dir.
+SLEEP_ON_COOLDOWN='True' # If you're issued a cooldown for quota limits from Google, the script will sleep for a specified amount of time (Default=25hrs).
 
 #Database connection - DO NOT CHANGE unless you know what you're doing. Connection info should be placed in .env
-DatabaseConnection =os.getenv("DatabaseConnection")
+DatabaseConnection=os.getenv("DatabaseConnection")
 
 #API KEY Linking - DO NOT CHANGE unless you know what you're doing. API Key should be placed in .env
 GOOGLE_CLOUD_API=os.getenv('GOOGLE_CLOUD_API')
